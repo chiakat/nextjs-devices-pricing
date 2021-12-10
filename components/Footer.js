f5cac0f8e8ef02405f418b7db734d42804d57b84
Katherine <chiakyu@gmail.com>
2021-12-09 16:42:35 -0800

    Refactor footer for DRY

--- components/Footer.js
+++ components/Footer.js
@@ -21,6 +21,95 @@ const iconStyle = {
   },
 };
 
+const externalSites = [
+  {
+    link: 'https://facebook.com/',
+    icon: <FacebookIcon />,
+  },
+  {
+    link: 'https://twitter.com/',
+    icon: <TwitterIcon />,
+  },
+  {
+    link: 'https://github.com/',
+    icon: <GitHubIcon />,
+  },
+  {
+    link: 'https://facebook.com/',
+    icon: <FacebookIcon />,
+  },
+  {
+    link: 'https://github.com/',
+    icon: <GitHubIcon />,
+  },
+];
+
+const footerLinks = [
+  {
+    header: 'Overview',
+    subHeaders: [
+      {
+        label: 'Products',
+        path: '/',
+      },
+      {
+        label: 'Use Cases',
+        path: '/',
+      },
+      {
+        label: 'Pricing',
+        path: '/pricing',
+      },
+      {
+        label: 'Blog',
+        path: '/',
+      },
+    ],
+  },
+  {
+    header: 'Resources',
+    subHeaders: [
+      {
+        label: 'Docs',
+        path: '/',
+      },
+      {
+        label: 'Model Catalog',
+        path: '/',
+      },
+      {
+        label: 'Dashboard',
+        path: '/',
+      },
+      {
+        label: 'Terms of Use',
+        path: '/',
+      },
+    ],
+  },
+  {
+    header: 'Company',
+    subHeaders: [
+      {
+        label: 'About',
+        path: 'https://alwaysai.co/',
+      },
+      {
+        label: 'Careers',
+        path: '/',
+      },
+      {
+        label: 'Press',
+        path: '/',
+      },
+      {
+        label: 'Privacy',
+        path: '/',
+      },
+    ],
+  },
+];
+
 function Logo() {
   return (
     <React.Fragment>
@@ -36,33 +125,11 @@ function Logo() {
         sx={{ height: 100 }}
       >
         <Grid item sx={{ display: 'flex' }}>
-          <Box component="a" href="https://facebook.com/" sx={iconStyle}>
-            <FacebookIcon />
-          </Box>
-          <Box
-            component="a"
-            href="https://twitter.com/"
-            sx={iconStyle}
-          >
-            <TwitterIcon />
-          </Box>
-          <Box
-            component="a"
-            href="https://github.com/"
-            sx={iconStyle}
-          >
-            <GitHubIcon />
-          </Box>
-          <Box component="a" href="https://facebook.com/" sx={iconStyle}>
-            <FacebookIcon />
-          </Box>
-          <Box
-            component="a"
-            href="https://github.com/"
-            sx={iconStyle}
-          >
-            <GitHubIcon />
-          </Box>
+          {externalSites.map((site) => (
+            <Box component="a" key={site.link} href={site.link} sx={iconStyle}>
+              {site.icon}
+            </Box>
+          ))}
         </Grid>
       </Grid>
     </React.Fragment>
@@ -80,86 +147,45 @@ export default function Footer() {
           <Grid item sx={{ minWidth: 200, mr: 1 }} xs={6} sm={4} md={3}>
             <Logo />
           </Grid>
-            <Grid item xs={6} sm={4} md={2}>
+          {footerLinks.map((link) => (
+            <Grid key={link.header} item xs={6} sm={4} md={2}>
               <Typography variant="h5" marked="left" color="white" gutterBottom>
-                Overview
+                {link.header}
               </Typography>
               <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Products</Link>
+              {link.subHeaders.map((subHeader) => (
+                <Box key={subHeader.label} component="li" sx={{ py: 0.5 }}>
+                  <Link href={subHeader.path} underline='hover' sx={{ color: '#ffbebe' }}>
+                    {subHeader.label}
+                  </Link>
                 </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Use Cases</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Pricing</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Blog</Link>
-                </Box>
+              ))}
               </Box>
             </Grid>
-            <Grid item xs={6} sm={4} md={2}>
-              <Typography variant="h5" marked="left" color="white" gutterBottom>
-                Resources
-              </Typography>
-              <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Docs</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Model Catalog</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Dashboard</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Terms of Use</Link>
-                </Box>
+          ))}
+          <Grid item xs={6} sm={4} md={2}>
+            <Typography variant="h5" marked="left" color="white" gutterBottom>
+              Contact
+            </Typography>
+            <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
+              <Box component="li" sx={{ py: 0.5 }}>
+                <Typography color="#ffbebe">Support</Typography>
               </Box>
-            </Grid>
-            <Grid item xs={6} sm={4} md={2}>
-              <Typography variant="h5" marked="left" color="white" gutterBottom>
-                Company
-              </Typography>
-              <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>About</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Careers</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Press</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="https://alwaysai.co/" underline='hover' sx={{ color: '#ffbebe' }}>Privacy</Link>
-                </Box>
+              <Box component="li" sx={{ py: 0.5 }}>
+                <Typography color="#ffbebe">555-555-5555</Typography>
               </Box>
-            </Grid>
-            <Grid item xs={6} sm={4} md={2}>
-              <Typography variant="h5" marked="left" color="white" gutterBottom>
-                Contact
-              </Typography>
-              <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Typography color="#ffbebe">Support</Typography>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Typography color="#ffbebe">555-555-5555</Typography>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Link href="mailto:support@alwaysai.co" underline='hover' sx={{ color: '#ffbebe' }}>support@alwaysai.co</Link>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Typography color="#ffbebe">720 Stevens Ave #270</Typography>
-                </Box>
-                <Box component="li" sx={{ py: 0.5 }}>
-                  <Typography color="#ffbebe">Solana Beach, Ca 92075</Typography>
-                </Box>
+              <Box component="li" sx={{ py: 0.5 }}>
+                <Link href="mailto:support@alwaysai.co" underline='hover' sx={{ color: '#ffbebe' }}>support@alwaysai.co</Link>
               </Box>
-            </Grid>
+              <Box component="li" sx={{ py: 0.5 }}>
+                <Typography color="#ffbebe">720 Stevens Ave #270</Typography>
+              </Box>
+              <Box component="li" sx={{ py: 0.5 }}>
+                <Typography color="#ffbebe">Solana Beach, Ca 92075</Typography>
+              </Box>
+            </Box>
           </Grid>
+        </Grid>
       </Container>
     </Typography>
   );
